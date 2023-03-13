770303e3a0d26e760beaf4e032115f4ca06646dd
luukas-k <25417369+luukas-k@users.noreply.github.com>
2023-03-13 11:28:27 +0200

    Update renderer.

--- core/graphics/Renderer2D.cpp
+++ core/graphics/Renderer2D.cpp
@@ -6,96 +6,79 @@
 
 namespace Slick::Gfx {
 
-	struct vertex {
-		Math::fVec2 pos;
-		Math::fVec2 uv;
-	};
-
 	Renderer2D::Renderer2D() 
 		:
-		mScreen({0, 0, 0, 0})
-	{}
-
-	Renderer2D::~Renderer2D() {
-
+		mScreen({0, 0, 0, 0}),
+		mVertexCount(0),
+		mVertices({}),
+		mShader("shader/vs2d.glsl", "shader/fs2d.glsl"),
+		mVertexBuffer(0),
+		mCurrentVertexBufferSize(0)
+	{
+		glGenBuffers(1, &mVertexBuffer);
 	}
 
-	void Renderer2D::draw_rect(Math::fVec2 p0, Math::fVec2 p1, Math::fVec2 uv0, Math::fVec2 uv1, Math::fVec3 color) {
+	Renderer2D::~Renderer2D() {}
+
+	void Renderer2D::begin() {
+		mVertexCount = 0;
+	}
+
+	void Renderer2D::end() {
+		mShader.bind(); 
+
+		u32 vao{};
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		// Draw
+		glBindBuffer(GL_ARRAY_BUFFER, mVertexBuffer);
+		if (mCurrentVertexBufferSize != mVertices.size()) {
+			mCurrentVertexBufferSize = mVertices.size();
+			glBufferData(GL_ARRAY_BUFFER, mVertices.size() * sizeof(Vertex2D), mVertices.data(), GL_STATIC_DRAW);
+		}
+		else {
+			glBufferSubData(GL_ARRAY_BUFFER, 0, mVertexCount * sizeof(Vertex2D), mVertices.data());
+		}
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(Vertex2D), (const void*)offsetof(Vertex2D, pos));
+
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof(Vertex2D), (const void*)offsetof(Vertex2D, uv));
+		
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 3, GL_FLOAT, false, sizeof(Vertex2D), (const void*)offsetof(Vertex2D, color));
+		
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_BLEND);
+
+		glDrawArrays(GL_TRIANGLES, 0, mVertexCount);
+		
+		glDeleteVertexArrays(1, &vao);
+	}
+
+	void Renderer2D::submit_rect(Math::fVec2 p0, Math::fVec2 p1, Math::fVec2 uv0, Math::fVec2 uv1, Math::fVec3 color) {
+		auto push_vertex = [&](Vertex2D v) {
+			if (mVertexCount < mVertices.size()) {
+				mVertices[mVertexCount++] = v;
+			}
+			else {
+				mVertexCount++;
+				mVertices.push_back(v);
+			}
+		};
+
 		p0 = p0 * 2.f - Math::fVec2{1.f, 1.f};
 		p1 = p1 * 2.f - Math::fVec2{1.f, 1.f};
 
-		Shader shader("shader/vs2d.glsl", "shader/fs2d.glsl");
-		shader.bind();
+		push_vertex({ {p0.x, p0.y}, {uv0.x, uv0.y}, color });
+		push_vertex({ {p1.x, p1.y}, {uv1.x, uv1.y}, color });
+		push_vertex({ {p0.x, p1.y}, {uv0.x, uv1.y}, color });
 
-		glUniform3f(glGetUniformLocation(shader.id(), "sys_color"), color.x, color.y, color.z);
-		
-		u32 vao{}, vbo{};
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
-
-		glGenBuffers(1, &vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-		std::vector<vertex> data{
-			{ {p0.x, p0.y}, {uv0.x, uv0.y} },
-			{ {p1.x, p1.y}, {uv1.x, uv1.y} },
-			{ {p0.x, p1.y}, {uv0.x, uv1.y} },
-
-			{ {p1.x, p1.y}, {uv1.x, uv1.y} },
-			{ {p0.x, p0.y}, {uv0.x, uv0.y} },
-			{ {p1.x, p0.y}, {uv1.x, uv0.y} },
-		};
-
-		glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(vertex), data.data(), GL_STATIC_DRAW);
-
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(vertex), (const void*)offsetof(vertex, pos));
-
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof(vertex), (const void*)offsetof(vertex, uv));
-
-		glDrawArrays(GL_TRIANGLES, 0, (GLsizei)data.size());
-		
-		glDeleteBuffers(1, &vbo);
-		glDeleteVertexArrays(1, &vao);
-	}
-
-	void Renderer2D::draw_rect_textured(Math::fVec2 p0, Math::fVec2 p1, Math::fVec2 uv0, Math::fVec2 uv1, u32 texture) {
-		Shader shader("shader/vs2d.glsl", "shader/vs2dtex.glsl");
-
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glUniform1i(glGetUniformLocation(shader.id(), "sys_texture"), 0);
-		
-		u32 vao{}, vbo{};
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
-
-		glGenBuffers(1, &vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-		std::vector<vertex> data{
-			{ {p0.x, p0.y}, {uv0.x, uv0.y} },
-			{ {p0.x, p1.y}, {uv0.x, uv1.y} },
-			{ {p1.x, p1.y}, {uv1.x, uv1.y} },
-
-			{ {p1.x, p1.y}, {uv1.x, uv1.y} },
-			{ {p1.x, p0.y}, {uv1.x, uv0.y} },
-			{ {p0.x, p0.y}, {uv0.x, uv0.y} }
-		};
-
-		glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(vertex), data.data(), GL_STATIC_DRAW);
-
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(vertex), (const void*)offsetof(vertex, pos));
-
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof(vertex), (const void*)offsetof(vertex, uv));
-
-		glDrawArrays(GL_TRIANGLES, 0, (GLsizei)data.size());
-		
-		glDeleteBuffers(1, &vbo);
-		glDeleteVertexArrays(1, &vao);
+		push_vertex({ {p1.x, p1.y}, {uv1.x, uv1.y}, color });
+		push_vertex({ {p0.x, p0.y}, {uv0.x, uv0.y}, color });
+		push_vertex({ {p1.x, p0.y}, {uv1.x, uv0.y}, color });
 	}
 
 }
--- core/graphics/Renderer2D.h
+++ core/graphics/Renderer2D.h
@@ -5,19 +5,33 @@
 #include "Viewport.h"
 #include "math/Vec.h"
 
+#include "Shader.h"
+
 namespace Slick::Gfx {
+
+	struct Vertex2D {
+		Math::fVec2 pos;
+		Math::fVec2 uv;
+		Math::fVec3 color;
+	};
 
 	class Renderer2D {
 	public:
 		Renderer2D();
 		~Renderer2D();
 
-		void draw_rect(Math::fVec2 p0, Math::fVec2 p1, Math::fVec2 uv0, Math::fVec2 uv1, Math::fVec3 color);
-		void draw_rect_textured(Math::fVec2 p0, Math::fVec2 p1, Math::fVec2 uv0, Math::fVec2 uv1, u32 tex);
+		void begin();
+		void end();
+		
+		void submit_rect(Math::fVec2 p0, Math::fVec2 p1, Math::fVec2 uv0, Math::fVec2 uv1, Math::fVec3 color);
 
 		inline void on_resize(Viewport vp) { mScreen = vp; }
 	private:
 		Viewport mScreen;
+		u64 mVertexCount;
+		std::vector<Vertex2D> mVertices;
+		Shader mShader;
+		u32 mVertexBuffer, mCurrentVertexBufferSize;
 	};
 
 }
