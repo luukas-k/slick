7f65a85af171f00eaf68803656aa62db25178581
luukas-k <25417369+luukas-k@users.noreply.github.com>
2023-03-15 09:31:13 +0200

    Update 2d shaders.

--- shader/fs2d.glsl
+++ shader/fs2d.glsl
@@ -4,8 +4,11 @@ in vec2 fPos;
 in vec2 fUV;
 in vec3 fColor;
 in float fTextureIndex;
+in float fQuadAspectRatio;
+in float fBorderRadius;
 
 uniform sampler2D sys_textures[16];
+uniform vec2 sys_viewport = vec2(1,1);
 
 out vec4 rColor;
 
@@ -19,5 +22,17 @@ vec4 get_color(){
 }
 
 void main(){
+	// const float b_radius = 0.05;
+	float b_radius = fBorderRadius;
+
+	if(fUV.x < b_radius / fQuadAspectRatio && fUV.y < b_radius && distance(vec2(b_radius), fUV * vec2(fQuadAspectRatio, 1.0)) > b_radius)
+		discard;
+	if(fUV.x > (1.0 - b_radius / fQuadAspectRatio) && fUV.y < b_radius && distance(vec2(b_radius, b_radius), (vec2(1.0 - fUV.x, fUV.y)) * vec2(fQuadAspectRatio, 1.0)) > b_radius)
+		discard;
+	if(fUV.x < b_radius / fQuadAspectRatio && fUV.y > (1.0 - b_radius) && distance(vec2(b_radius), vec2(fUV.x, 1.0 - fUV.y) * vec2(fQuadAspectRatio, 1.0)) > b_radius)
+		discard;
+	if(fUV.x > (1.0 - b_radius / fQuadAspectRatio) && fUV.y > (1.0 - b_radius) && distance(vec2(b_radius, b_radius), (vec2(1.0 - fUV.x, 1.0 - fUV.y)) * vec2(fQuadAspectRatio, 1.0)) > b_radius)
+		discard;
+	
 	rColor = get_color();
 }
--- shader/vs2d.glsl
+++ shader/vs2d.glsl
@@ -4,11 +4,15 @@ in vec2 vPos;
 in vec2 vUV;
 in vec3 vColor;
 in float vTextureIndex;
+in float vQuadAspectRatio;
+in float vBorderRadius;
 
 out vec2 fPos;
 out vec2 fUV;
 out vec3 fColor;
 out float fTextureIndex;
+out float fQuadAspectRatio;
+out float fBorderRadius;
 
 void main(){
 	gl_Position = vec4(vPos, 0.0, 1.0);
@@ -16,5 +20,7 @@ void main(){
 	fUV = vUV;
 	fColor = vColor;
 	fTextureIndex = vTextureIndex;
+	fQuadAspectRatio = vQuadAspectRatio;
+	fBorderRadius = vBorderRadius;
 }
 
